716b338138f0df16349dd8b25da0cfdf0d8e6265
Nathaniel Lowy <244323911+nathaniel-lowy@users.noreply.github.com>
2026-01-28 19:48:44 -0500

    Implement basic counters array
    Holds four numbers, one for the day, week, month, and year. Every
    addition increases each.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import "./styles.css";
 
 let falls = [];
+let counters = [0,0,0,0];
 
 document.querySelector("#add-btn").addEventListener("click", addFall);
 
+function checkResets() {
+  const today = new Date();
+  console.log(today.getDay()); // 0-6, for example: Wednesday returns 3
+  // if (today.getDay)
+}
+
 function addFall() {
   falls.push(new Date());
+  checkResets();
+  counters = counters.map(counter => {
+    return counter = counter + 1;
+  });
   displayFalls();
 }
 
 function displayFalls() {
   const fallLog = document.querySelector(".logs");
+  const day = document.querySelector(".day");
+  const week = document.querySelector(".week");
+  const month = document.querySelector(".month");
   const year = document.querySelector(".year");
   const options = {
     weekday: 'short',
@@ -29,6 +43,8 @@ function displayFalls() {
     fallLog.prepend(fallCount);
   });
 
-  // NOTE: Day/Week/Month/Year calculations will need to be implemented
-  year.textContent = parseInt(year.textContent) + 1;
+  day.textContent = counters[0];
+  week.textContent = counters[1];
+  month.textContent = counters[2];
+  year.textContent = counters[3];
 }
