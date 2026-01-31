f5ba30a1fe0d56b196dcbdef68a3ed04982d0a1e
Nathaniel Lowy <244323911+nathaniel-lowy@users.noreply.github.com>
2026-01-31 15:21:37 -0500

    Incorporate daily/weekly/monthly/yearly counters
    Counters now reset at the appropriate times. This commit also
    allows renaming what is tracked, as well as saving data in local
    storage.

--- src/index.js
+++ src/index.js
@@ -1,39 +1,113 @@
 import "./styles.css";
 
 let falls = [];
-let counters = [0,0,0,0];
+// The below counters should push a new value in each new day/week/month/year,
+// and increment the latest position element each fall
+let days = [];
+let weeks = [];
+let months = [];
+let years = [];
+
+for (let i = 0; i < 365; i++) {
+  days.push(0);
+}
+for (let i = 0; i < 52; i++) {
+  weeks.push(0);
+}
+for (let i = 0; i < 12; i++) {
+  months.push(0);
+}
+for (let i = 0; i < 200; i++) {
+  years.push(0);
+}
 
 document.querySelector("#add-btn").addEventListener("click", addFall);
 
-function checkResets() {
-  const today = new Date();
-  console.log(today.getDay()); // 0-6, for example: Wednesday returns 3
-  // if (today.getDay)
-}
+// Will later have to set up functionality so that every additional fall
+// updates the appropriate array counter, so that a long gap does not affect
+// accuracy. Such as: If a week is skipped, that week's counter was already
+// updated, so there is no need to check if there's been a gap.
+// function checkResets() {
+//   const today = new Date();
+//   const last = falls.at(-1);
+
+//   // Check day
+//   if (today.getDate() > last.getDate()) {
+//     counters[0] = 0;
+//   }
+
+//   // Check week using the getWeek function from weeknumber.com (attributed below)
+//   if (today.getWeek() > last.getWeek()) {
+//     counters[1] = 0;
+//   }
+
+//   // Check month
+//   if (today.getMonth() > last.getMonth()) {
+//     counters[2] = 0;
+//   }
+
+//   // Check year
+//   if (today.getFullYear() > last.getFullYear()) {
+//     counters[3] = 0;
+//   }
+
+//   console.log(today.getDate());
+//   console.log(today.getWeek());
+//   console.log(today.getMonth());
+//   console.log("Year", today.getFullYear());
+// }
+
+// This script is released to the public domain and may be used, modified and
+// distributed without restrictions. Attribution not necessary but appreciated.
+// Source: https://weeknumber.com/how-to/javascript
+
+// Returns the ISO week of the date.
+Date.prototype.getWeek = function () {
+  var date = new Date(this.getTime());
+  date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return (
+    1 +
+    Math.round(
+      ((date.getTime() - week1.getTime()) / 86400000 -
+        3 +
+        ((week1.getDay() + 6) % 7)) /
+        7,
+    )
+  );
+};
 
 function addFall() {
+  const today = new Date();
   falls.push(new Date());
-  checkResets();
-  counters = counters.map(counter => {
-    return counter = counter + 1;
-  });
+
+  days[today.getDate()] = days[today.getDate()] + 1;
+  weeks[today.getWeek()] = weeks[today.getWeek()] + 1;
+  months[today.getMonth()] = months[today.getMonth()] + 1;
+  years[today.getFullYear() - 2026] = years[today.getFullYear() - 2026] + 1;
+  save();
   displayFalls();
 }
 
 function displayFalls() {
+  const today = new Date();
   const fallLog = document.querySelector(".logs");
   const day = document.querySelector(".day");
   const week = document.querySelector(".week");
   const month = document.querySelector(".month");
   const year = document.querySelector(".year");
   const options = {
-    weekday: 'short',
-    month: 'short',
-    day: 'numeric',
-    year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric'
-  }
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
   fallLog.replaceChildren();
   falls.forEach((fall) => {
     const dateOutput = new Intl.DateTimeFormat("en-us", options).format(fall);
@@ -43,8 +117,36 @@ function displayFalls() {
     fallLog.prepend(fallCount);
   });
 
-  day.textContent = counters[0];
-  week.textContent = counters[1];
-  month.textContent = counters[2];
-  year.textContent = counters[3];
+  day.textContent = days[today.getDate()];
+  week.textContent = weeks[today.getWeek()];
+  month.textContent = months[today.getMonth()];
+  year.textContent = years[today.getFullYear() - 2026];
+}
+
+load();
+displayFalls();
+
+function save() {
+  localStorage.setItem("fall-counter-heading", JSON.stringify(document.querySelector("h3").textContent));
+  localStorage.setItem("fall-counter-falls", JSON.stringify(falls));
+  localStorage.setItem("fall-counter-days", JSON.stringify(days));
+  localStorage.setItem("fall-counter-weeks", JSON.stringify(weeks));
+  localStorage.setItem("fall-counter-months", JSON.stringify(months));
+  localStorage.setItem("fall-counter-years", JSON.stringify(years));
+}
+
+function load() {
+  console.log("load called")
+  if (localStorage.getItem("fall-counter-falls") !== null) {
+    const fallStrings = JSON.parse(localStorage.getItem("fall-counter-falls"));
+    document.querySelector("h3").textContent = JSON.parse(localStorage.getItem("fall-counter-heading"));
+    days = JSON.parse(localStorage.getItem("fall-counter-days"));
+    weeks = JSON.parse(localStorage.getItem("fall-counter-weeks"));
+    months = JSON.parse(localStorage.getItem("fall-counter-months"));
+    years = JSON.parse(localStorage.getItem("fall-counter-years"));
+
+    fallStrings.forEach(fall => {
+      falls.push(new Date(fall));
+    })
+  }
 }
